06e3c9ab742158e8f5262f309286e656554cd145
sdfsfd <sdfd@sdfsd.ff>
2020-03-20 20:42:08 +0300

    Updates

---
+++ precache-manifest.f387408a5acf77c300233e85127569a1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f16485149fea65f1dd374d3a1a81893",
+    "url": "/xOPS/index.html"
+  },
+  {
+    "revision": "3726a3b420953fbcb988",
+    "url": "/xOPS/static/css/main.2117ac9d.chunk.css"
+  },
+  {
+    "revision": "e2f63d3b3a26080eaf25",
+    "url": "/xOPS/static/js/2.be03c727.chunk.js"
+  },
+  {
+    "revision": "4d7ce9f5437c7cb260881d08dc78d7a9",
+    "url": "/xOPS/static/js/2.be03c727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3726a3b420953fbcb988",
+    "url": "/xOPS/static/js/main.e5b4b7d4.chunk.js"
+  },
+  {
+    "revision": "b7927446842458d0af14",
+    "url": "/xOPS/static/js/runtime-main.d71d1c20.js"
+  },
+  {
+    "revision": "dd31905cb4394b5965adae9cb2bfd25c",
+    "url": "/xOPS/static/media/EnBlack.dd31905c.png"
+  },
+  {
+    "revision": "bebad3b9bf225cdecc7b9fe714f3b46d",
+    "url": "/xOPS/static/media/EnWhite.bebad3b9.png"
+  },
+  {
+    "revision": "036d007395279645d7dd332ac08e6c66",
+    "url": "/xOPS/static/media/IBMPlexMono-Italic.036d0073.woff2"
+  },
+  {
+    "revision": "97aeea180bd7d5694ecdb8998ea9d275",
+    "url": "/xOPS/static/media/IBMPlexMono-Italic.97aeea18.woff"
+  },
+  {
+    "revision": "70b59c39c9071242baa75908c9796d0e",
+    "url": "/xOPS/static/media/IBMPlexMono-Medium.70b59c39.woff"
+  },
+  {
+    "revision": "d55f91bf0056794f4d2ad74327011e67",
+    "url": "/xOPS/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
+  },
+  {
+    "revision": "27a3afd6a029d53608f80f2ee809f614",
+    "url": "/xOPS/static/media/IBMPlexMono-Regular.27a3afd6.woff2"
+  },
+  {
+    "revision": "5632af57e4d486d7597865250c6b1e77",
+    "url": "/xOPS/static/media/IBMPlexMono-Regular.5632af57.woff"
+  },
+  {
+    "revision": "37698cdd304ef06da08b86889e5593b5",
+    "url": "/xOPS/static/media/IBMPlexMono-SemiBold.37698cdd.woff"
+  },
+  {
+    "revision": "6624977e05316ed6e27ee2a32738ab7e",
+    "url": "/xOPS/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
+  },
+  {
+    "revision": "bd379b4108d626229acca4a4cf8f66b6",
+    "url": "/xOPS/static/media/RuBlack.bd379b41.png"
+  },
+  {
+    "revision": "47b2b792238f5cf6b4a26b91f1e65456",
+    "url": "/xOPS/static/media/RuWhite.47b2b792.png"
+  },
+  {
+    "revision": "a00fa3a4c76271f16b9808841091a2b6",
+    "url": "/xOPS/static/media/apk.a00fa3a4.svg"
+  },
+  {
+    "revision": "990c2b02425e4ee8299a18b645f267b5",
+    "url": "/xOPS/static/media/github.990c2b02.svg"
+  },
+  {
+    "revision": "7041ae46fb80f1678b2c020235176f33",
+    "url": "/xOPS/static/media/google-play.7041ae46.svg"
+  },
+  {
+    "revision": "51ffa359ccbd41bedabbf079148d7858",
+    "url": "/xOPS/static/media/linux.51ffa359.svg"
+  },
+  {
+    "revision": "3a236d88e7bd2ce0636f60bd1dcc5b7d",
+    "url": "/xOPS/static/media/macos.3a236d88.svg"
+  },
+  {
+    "revision": "15e725319135ac27002d0751851a978b",
+    "url": "/xOPS/static/media/windows.15e72531.svg"
+  }
+]);
